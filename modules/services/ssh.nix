f1563551f3de326e28bd3345626daa436960030b
A1ca7raz <7345998+A1ca7raz@users.noreply.github.com>
2024-07-13 17:10:38 +0800

    openssh: remove user ca

--- modules/services/ssh.nix
+++ modules/services/ssh.nix
@@ -13,9 +13,9 @@ in {
       { path = "/etc/ssh/ssh_host_ed25519_key"; type = "ed25519"; }
       { path = "/etc/ssh/ssh_host_rsa_key"; type = "rsa"; }
     ];
-    extraConfig = ''
-      TrustedUserCAKeys /etc/ssh/ssh_user_ca.pub
-    '';
+    # extraConfig = ''
+    #   TrustedUserCAKeys /etc/ssh/ssh_user_ca.pub
+    # '';
   };
 
   environment.persistence."/nix/persist".files = [
