e5ca863344dd688afcd3441c399a73d85302c3a5
A1ca7raz <7345998+A1ca7raz@users.noreply.github.com>
2024-07-14 16:03:21 +0800

    openssh: update sshkey

--- modules/config/root.nix
+++ modules/config/root.nix
@@ -4,7 +4,7 @@
   users.users.root = {
     hashedPassword = "$6$EzkF9f7txOPfTWuP$Qqu8qRs2kQBz079PxXELkqjpdH/eIJuV6FMsAzT7sZRFJc/sgdNfGUPDkjmj2fTx5pLZb24seyLFr7.q4hYWd0";
     openssh.authorizedKeys.keys = [
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILeHR3VQCaMfrmY2TErvQOattia/c6eKT3/Bt/iAuVeT"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOu5O5fSfPedhXBbxpRbSTu13J/+gk0j0EaPFVzzwwFi"
     ];
   };
 
